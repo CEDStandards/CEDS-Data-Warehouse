23b1432553e934b8cc9a371e0c1329b6a03d23e6
Duane Brown <duane.brown@aemcorp.com>
2023-08-10 23:59:20 -0500

    Add additional metadata to the ddl script

--- src/ddl/CEDS-Data-Warehouse-V11.0.0.0.sql
+++ src/ddl/CEDS-Data-Warehouse-V11.0.0.0.sql
@@ -43898,3 +43898,23 @@ EXEC sys.sp_addextendedproperty @name=N'CEDS_URL', @value=N'https://ceds.ed.gov/
 GO
 EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'See the CEDS_GlobalId, CEDS_Element, CEDS_URL, and CEDS_Def_Desc extended properties.' , @level0type=N'SCHEMA',@level0name=N'Staging', @level1type=N'TABLE',@level1name=N'StateDetail', @level2type=N'COLUMN',@level2name=N'DataCollectionName'
 GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'See the CEDS_GlobalId, CEDS_Element, CEDS_URL, and CEDS_Def_Desc extended properties.' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EnrollmentEntryDateId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_Def_Desc', @value=N'The month, day, and year on which a person enters and begins to receive instructional services in a school, institution, program, or class-section during a given session.' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EnrollmentEntryDateId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_Element', @value=N'Enrollment Entry Date' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EnrollmentEntryDateId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_GlobalId', @value=N'000097' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EnrollmentEntryDateId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_URL', @value=N'https://ceds.ed.gov/CEDSElementDetails.aspx?TermId=21097' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EnrollmentEntryDateId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'See the CEDS_GlobalId, CEDS_Element, CEDS_URL, and CEDS_Def_Desc extended properties.' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EntryGradeLevelId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_Def_Desc', @value=N'The grade level or primary instructional level at which a student enters and receives services in a school or an educational institution during a given academic session.' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EntryGradeLevelId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_Element', @value=N'Entry Grade Level' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EntryGradeLevelId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_GlobalId', @value=N'000100' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EntryGradeLevelId';
+GO
+EXEC sys.sp_addextendedproperty @name=N'CEDS_URL', @value=N'https://ceds.ed.gov/CEDSElementDetails.aspx?TermId=21100' , @level0type=N'SCHEMA',@level0name=N'RDS', @level1type=N'TABLE',@level1name=N'FactK12StudentEnrollments', @level2type=N'COLUMN',@level2name=N'EntryGradeLevelId';
+GO
