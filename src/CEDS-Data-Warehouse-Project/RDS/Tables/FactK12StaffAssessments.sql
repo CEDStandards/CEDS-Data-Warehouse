66e0d0fc7b9e1c7939a9be2f4ba72f4482c7f720
Nathan Clinton <nathan.clinton@aemcorp.com>
2024-03-25 15:31:21 -0700

    Additional fields required by Michigan to support new staff data collection.

--- src/CEDS-Data-Warehouse-Project/RDS/Tables/FactK12StaffAssessments.sql
+++ src/CEDS-Data-Warehouse-Project/RDS/Tables/FactK12StaffAssessments.sql
@@ -1,38 +1,41 @@
 CREATE TABLE [RDS].[FactK12StaffAssessments]
 (
 	[FactK12StaffAssessmentId] int NOT NULL IDENTITY (1, 1),
-	[CredentialAwardStatusId] int NULL,
-	[EmployerId] int NULL,
-	[DataCollectionId] int NULL,
-	[K12EmploymentStatusId] int NULL,
-	[K12DemographicId] int NULL,
-	[K12StaffCategoryId] int NULL,
-	[K12StaffStatusId] int NULL,
-	[K12StaffPersonId] bigint NULL,
-	[CredentialIssuerId] int NULL,
-	[SchoolYearId] int NULL,
-	[StandardOccupationalClassificationId] int NULL,
-	[CredentialDefinitionId] int NULL,
-	[AssessmentId] int NULL,
-	[AssessmentSubtestId] int NULL,
-	[SeaJobClassificationId] int NULL,
-	[DimAssessmentParticipationSessionId] int NULL,
-	[AssessmentAccommodationId] int NULL,
-	[AssessmentFormId] int NULL,
-	[AssessmentAdministrationId] int NULL,
-	[AssessmentRegistrationId] int NULL,
-	[AssessmentPerformanceLevelId] int NULL,
-	[OnetSocOccupationTypeId] int NULL,
-	[K12PositionId] int NULL,
-	[K12StaffAssignmentStatusId] int NULL,
-	[K12JobId] int NULL,
-	[AssessmentResultScoreValueRawScore] nvarchar(70) NULL,
-	[CredentialAwardId] int NULL,
-	[AssessmentResultScoreValueScaleScore] nvarchar(70) NULL,
-	[AssessmentResultScoreValuePercentile] nvarchar(70) NULL,
-	[AssessmentResultScoreValueTScore] nvarchar(70) NULL,
-	[AssessmentResultScoreValueZScore] nvarchar(70) NULL,
-	[LeaJobClassificationId] int NULL,
+	[SchoolYearId] 											INT 			CONSTRAINT [DF_FactK12StaffAssessments_SchoolYearId] DEFAULT ((-1)) NOT NULL,
+	[DataCollectionId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_DataCollectionId] DEFAULT ((-1)) NOT NULL,
+	[K12StaffPersonId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12StaffPersonId] DEFAULT ((-1)) NOT NULL,
+	[PsInstitutionId] 										BIGINT 			CONSTRAINT [DF_FactK12StaffAssessments_PsInstitutionId] DEFAULT ((-1)) NOT NULL,
+	[EmployerId] 											INT 			CONSTRAINT [DF_FactK12StaffAssessments_EmployerId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentId] 											INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentAccommodationId] 							INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentAccommodationId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentAdministrationId] 							INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentAdministrationId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentFormId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentFormId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentParticipationSessionId] 						INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentParticipationSessionId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentPerformanceLevelId] 							INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentPerformanceLevelId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentRegistrationId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentRegistrationId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentRegistrationGradeLevelToBeAssessedId] 		INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentRegistrationGradeLevelToBeAssessedId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentSubtestId] 									INT 			CONSTRAINT [DF_FactK12StaffAssessments_AssessmentSubtestId] DEFAULT ((-1)) NOT NULL,
+	[CredentialAwardId] 									INT 			CONSTRAINT [DF_FactK12StaffAssessments_CredentialAwardId] DEFAULT ((-1)) NOT NULL,
+	[CredentialAwardStatusId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_CredentialAwardStatusId] DEFAULT ((-1)) NOT NULL,
+	[CredentialDefinitionId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_CredentialDefinitionId] DEFAULT ((-1)) NOT NULL,
+	[CredentialIssuerId] 									INT 			CONSTRAINT [DF_FactK12StaffAssessments_CredentialIssuerId] DEFAULT ((-1)) NOT NULL,
+	[K12DemographicId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12DemographicId] DEFAULT ((-1)) NOT NULL,
+	[K12EmploymentStatusId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12EmploymentStatusId] DEFAULT ((-1)) NOT NULL,
+	[K12JobId] 												INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12JobId] DEFAULT ((-1)) NOT NULL,
+	[K12PositionId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12PositionId] DEFAULT ((-1)) NOT NULL,
+	[K12StaffAssignmentStatusId] 							INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12StaffAssignmentStatusId] DEFAULT ((-1)) NOT NULL,
+	[K12StaffCategoryId] 									INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12StaffCategoryId] DEFAULT ((-1)) NOT NULL,
+	[K12StaffStatusId] 										INT 			CONSTRAINT [DF_FactK12StaffAssessments_K12StaffStatusId] DEFAULT ((-1)) NOT NULL,
+	[LeaJobClassificationId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_LeaJobClassificationId] DEFAULT ((-1)) NOT NULL,
+	[OnetSocOccupationTypeId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_OnetSocOccupationTypeId] DEFAULT ((-1)) NOT NULL,
+	[SeaJobClassificationId] 								INT 			CONSTRAINT [DF_FactK12StaffAssessments_SeaJobClassificationId] DEFAULT ((-1)) NOT NULL,
+	[StandardOccupationalClassificationId] 					INT 			CONSTRAINT [DF_FactK12StaffAssessments_StandardOccupationalClassificationId] DEFAULT ((-1)) NOT NULL,
+	[AssessmentResultScoreValueRawScore] 					NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValueRawScore] DEFAULT ((0)) NOT NULL,
+	[AssessmentResultScoreValueScaleScore] 					NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValueScaleScore] DEFAULT ((0)) NOT NULL,
+	[AssessmentResultScoreValuePassFail] 					NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValuePassFail] DEFAULT ((0)) NOT NULL,
+	[AssessmentResultScoreValuePercentile] 					NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValuePercentile] DEFAULT ((0)) NOT NULL,
+	[AssessmentResultScoreValueTScore] 						NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValueTScore] DEFAULT ((0)) NOT NULL,
+	[AssessmentResultScoreValueZScore] 						NVARCHAR (70) 	CONSTRAINT [DF_FactK12StaffAssessments_AssessmentResultScoreValueZScore] DEFAULT ((0)) NOT NULL,
 	CONSTRAINT [PK_FactK12StaffAssessments]	PRIMARY KEY CLUSTERED ([FactK12StaffAssessmentId] ASC),
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentAccommodationId] FOREIGN KEY ([AssessmentAccommodationId]) REFERENCES [RDS].[DimAssessmentAccommodations] ([DimAssessmentAccommodationId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentAdministrationId] FOREIGN KEY ([AssessmentAdministrationId]) REFERENCES [RDS].[DimAssessmentAdministrations] ([DimAssessmentAdministrationId]) ON DELETE No Action ON UPDATE No Action,
@@ -40,13 +43,14 @@ CREATE TABLE [RDS].[FactK12StaffAssessments]
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentId] FOREIGN KEY ([AssessmentId]) REFERENCES [RDS].[DimAssessments] ([DimAssessmentId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentPerformanceLevelId] FOREIGN KEY ([AssessmentPerformanceLevelId]) REFERENCES [RDS].[DimAssessmentPerformanceLevels] ([DimAssessmentPerformanceLevelId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentRegistrationId] FOREIGN KEY ([AssessmentRegistrationId]) REFERENCES [RDS].[DimAssessmentRegistrations] ([DimAssessmentRegistrationId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentRegistrationGradeLevelToBeAssessedId] FOREIGN KEY ([AssessmentRegistrationGradeLevelToBeAssessedId]) REFERENCES [RDS].[DimGradeLevels] ([AssessmentRegistrationGradeLevelToBeAssessedId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentSubtestId]FOREIGN KEY ([AssessmentSubtestId]) REFERENCES [RDS].[DimAssessmentSubtests] ([DimAssessmentSubtestId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_CredentialAwardId]	FOREIGN KEY ([CredentialAwardId]) REFERENCES [RDS].[DimCredentialAwards] ([DimCredentialAwardId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_CredentialAwardStatusId] FOREIGN KEY ([CredentialAwardStatusId]) REFERENCES [RDS].[DimCredentialAwardStatuses] ([DimCredentialAwardStatusId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_CredentialDefinitionId] FOREIGN KEY ([CredentialDefinitionId]) REFERENCES [RDS].[DimCredentialDefinitions] ([DimCredentialDefinitionId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_CredentialIssuerId] FOREIGN KEY ([CredentialIssuerId]) REFERENCES [RDS].[DimCredentialIssuers] ([DimCredentialIssuerId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_DataCollectionId] FOREIGN KEY ([DataCollectionId]) REFERENCES [RDS].[DimDataCollections] ([DimDataCollectionId]) ON DELETE No Action ON UPDATE No Action,
-	CONSTRAINT [FK_FactK12StaffAssessments_DimAssessmentParticipationSessionId] FOREIGN KEY ([DimAssessmentParticipationSessionId]) REFERENCES [RDS].[DimAssessmentParticipationSessions] ([DimAssessmentParticipationSessionId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12StaffAssessments_AssessmentParticipationSessionId] FOREIGN KEY ([AssessmentParticipationSessionId]) REFERENCES [RDS].[DimAssessmentParticipationSessions] ([AssessmentParticipationSessionId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_EmployerId] FOREIGN KEY ([EmployerId]) REFERENCES [RDS].[DimEmployers] ([DimEmployerId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_K12DemographicId] FOREIGN KEY ([K12DemographicId]) REFERENCES [RDS].[DimK12Demographics] ([DimK12DemographicId]) ON DELETE No Action ON UPDATE No Action,
 	CONSTRAINT [FK_FactK12StaffAssessments_K12EmploymentStatusId] FOREIGN KEY ([K12EmploymentStatusId]) REFERENCES [RDS].[DimK12EmploymentStatuses] ([DimK12EmploymentStatusId]) ON DELETE No Action ON UPDATE No Action,
@@ -76,7 +80,7 @@ CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimAssessmentForms]
 GO
 
 CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimAssessmentParticipationSessions] 
- ON [RDS].[FactK12StaffAssessments] ([DimAssessmentParticipationSessionId] ASC)
+ ON [RDS].[FactK12StaffAssessments] ([AssessmentParticipationSessionId] ASC)
 GO
 
 CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimAssessmentPerformanceLevels] 
@@ -157,6 +161,10 @@ GO
 
 CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimPeople] 
  ON [RDS].[FactK12StaffAssessments] ([K12StaffPersonId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimPsInstitutions] 
+ ON [RDS].[FactK12StaffAssessments] ([PsInstitutionId] ASC)
 GO
 
 CREATE NONCLUSTERED INDEX [IXFK_FactK12StaffAssessments_DimSchoolYears] 
