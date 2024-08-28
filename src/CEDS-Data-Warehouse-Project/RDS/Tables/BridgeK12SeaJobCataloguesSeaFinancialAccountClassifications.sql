481313acdb2207d5cf609120cf6003c9802de93d
Duane Brown <duane.brown@aemcorp.com>
2024-08-28 13:41:29 -0500

    Creation of Bridge tables for funding source and creation of new Fact table FactK12SeaJobCatalogues.

---
+++ src/CEDS-Data-Warehouse-Project/RDS/Tables/BridgeK12SeaJobCataloguesSeaFinancialAccountClassifications.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [RDS].[BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications]
+(
+	[BridgeK12SeaJobCatalogueSeaFinancialAccountClassificationId] int NOT NULL IDENTITY (1, 1),
+	[FactK12SeaJobCatalogueId] BIGINT NULL,
+	[SeaFinancialAccountClassificationId] int NULL,
+    [FundingProgramAllowed]    NVARCHAR (50)  NOT NULL,
+	CONSTRAINT [PK_BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications] PRIMARY KEY CLUSTERED ([BridgeK12SeaJobCatalogueSeaFinancialAccountClassificationId] ASC),
+	CONSTRAINT [FK_BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications_FactK12SeaJobCatalogueId] FOREIGN KEY ([FactK12SeaJobCatalogueId]) REFERENCES [RDS].[FactK12SeaJobCatalogues] ([FactK12SeaJobCatalogueId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications_SeaFinancialAccountClassificationId] FOREIGN KEY ([SeaFinancialAccountClassificationId]) REFERENCES [RDS].[DimSeaFinancialAccountClassifications] ([DimSeaFinancialAccountClassificationId]) ON DELETE No Action ON UPDATE No Action
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications_DimSeaFinancialAccountClassifications] 
+ ON [RDS].[BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications] ([SeaFinancialAccountClassificationId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications_FactK12SeaJobCatalogues] 
+ ON [RDS].[BridgeK12SeaJobCatalogueSeaFinancialAccountClassifications] ([FactK12SeaJobCatalogueId] ASC)
+GO
+
---
+++ src/CEDS-Data-Warehouse-Project/RDS/Tables/FactK12SeaJobCatalogues.sql
@@ -0,0 +1,87 @@
+CREATE TABLE [RDS].[FactK12SeaJobCatalogues]
+(
+	[FactK12SeaJobCatalogueId] int NOT NULL IDENTITY (1, 1),
+	[SchoolYearId] int NOT NULL,
+	[CountDateId] int NOT NULL,
+	[RecordStartDateTime] datetime NOT NULL,
+	[RecordEndDateTime] datetime NULL,
+	[OnetSocOccupationTypeId] int NOT NULL,
+	[StandardOccupationalClassificationId] int NOT NULL,
+	[K12StaffCategoryId] int NOT NULL,
+	[K12JobId] int NOT NULL,
+	[SeaJobClassificationId] int NOT NULL,
+	[DataCollectionId] int NOT NULL,
+	[RecordStatusId] int NOT NULL,
+
+
+	CONSTRAINT [PK_FactK12SeaJobCatalogues] PRIMARY KEY CLUSTERED ([FactK12SeaJobCatalogueId] ASC),
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_SchoolYearId] FOREIGN KEY ([SchoolYearId]) REFERENCES [RDS].[DimSchoolYears] ([DimSchoolYearId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_CountDateId] FOREIGN KEY ([CountDateId]) REFERENCES [RDS].[DimDates] ([DimDateId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_OnetSocOccupationTypeId] FOREIGN KEY ([OnetSocOccupationTypeId]) REFERENCES [RDS].[DimOnetSocOccupationTypes] ([DimOnetSocOccupationTypeId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_StandardOccupationalClassificationId] FOREIGN KEY ([StandardOccupationalClassificationId]) REFERENCES [RDS].[DimStandardOccupationalClassifications] ([DimStandardOccupationalClassificationId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_K12StaffCategoryId] FOREIGN KEY ([K12StaffCategoryId]) REFERENCES [RDS].[DimK12StaffCategories] ([DimK12StaffCategoryId]) ON DELETE No Action ON UPDATE No Action,
+    CONSTRAINT [FK_FactK12SeaJobCatalogues_K12JobId] FOREIGN KEY ([K12JobId]) REFERENCES [RDS].[DimK12Jobs] ([DimK12JobId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_SeaJobClassificationId] FOREIGN KEY ([SeaJobClassificationId]) REFERENCES [RDS].[DimSeaJobClassifications] ([DimSeaJobClassificationId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_DataCollectionId] FOREIGN KEY ([DataCollectionId]) REFERENCES [RDS].[DimDataCollections] ([DimDataCollectionId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_FactK12SeaJobCatalogues_RecordStatusId] FOREIGN KEY ([RecordStatusId]) REFERENCES [RDS].[DimRecordStatuses] ([DimRecordStatusId]) ON DELETE No Action ON UPDATE No Action,
+	
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_DataCollectionId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([DataCollectionId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_CountDateId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([CountDateId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_EmploymentEndDateId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([EmploymentEndDateId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_EmploymentStartDateId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([EmploymentStartDateId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_K12JobId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([K12JobId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_K12StaffCategoryId]
+    ON [RDS].[FactK12SeaJobCatalogues] ([K12StaffCategoryId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_OnetSocOccupationTypeId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([OnetSocOccupationTypeId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_RecordStatusId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([RecordStatusId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_SchoolYearId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([SchoolYearId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_StandardOccupationalClassificationId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([StandardOccupationalClassificationId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_SeaJobClassificationId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([SeaJobClassificationId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_RecordStartDateTime] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([RecordStartDateTime] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_RecordEndDateTime] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([RecordEndDateTime] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_FactK12SeaJobCatalogues_FactK12SeaJobCatalogueId] 
+ ON [RDS].[FactK12SeaJobCatalogues] ([FactK12SeaJobCatalogueId] ASC)   
+GO
+
+
