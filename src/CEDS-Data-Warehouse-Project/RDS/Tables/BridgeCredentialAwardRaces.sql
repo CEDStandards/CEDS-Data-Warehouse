c7d3884882723c81c362a31cd4412b6fe0089956
Nathan Clinton <nathan.clinton@aemcorp.com>
2025-12-17 15:40:51 -0800

    Resolves issue #123

---
+++ src/CEDS-Data-Warehouse-Project/RDS/Tables/BridgeCredentialAwardRaces.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [SOM].[BridgeCredentialAwardRaces]
+(
+	[BridgeCredentialAwardRaceId] int NOT NULL,
+	[FactCredentialAwardId] int NULL,
+	[RaceId] int NULL,
+
+	CONSTRAINT [PK_BridgeCredentialAwardRaces] PRIMARY KEY CLUSTERED ([BridgeCredentialAwardRaceId] ASC),
+	CONSTRAINT [FK_BridgeCredentialAwardRaces_FactCredentialAwardId] FOREIGN KEY ([FactCredentialAwardId]) REFERENCES [RDS].[FactCredentialAwards] ([FactCredentialAwardId]) ON DELETE No Action ON UPDATE No Action,
+	CONSTRAINT [FK_BridgeCredentialAwardRaces_RaceId] FOREIGN KEY ([RaceId]) REFERENCES [RDS].[DimRaces] ([DimRaceId]) ON DELETE No Action ON UPDATE No Action
+
+)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_BridgeCredentialAwardRaces_FactCredentialAwardId] 
+ ON [SOM].[BridgeCredentialAwardRaces] ([FactCredentialAwardId] ASC)
+GO
+
+CREATE NONCLUSTERED INDEX [IXFK_BridgeCredentialAwardRaces_RaceId] 
+ ON [SOM].[BridgeCredentialAwardRaces] ([RaceId] ASC)
+GO
